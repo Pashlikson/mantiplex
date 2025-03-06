b812a8c83e802f1c6cc4eebf8cc153f677eefbe1
Danyon-command <onchul.danylo@gmail.com>
2025-03-06 21:20:45 +0200

    feat: git push origin main

--- main/views.py
+++ main/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-
     return render(request, 'main.html') 
+
+def login_page(request):
+    return render(request, 'login_page.html')
