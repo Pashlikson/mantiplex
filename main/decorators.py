8682c7a9908930bf8e5fd7fcdf4f5795ee3e136f
Danyon-command <onchul.danylo@gmail.com>
2025-03-19 18:56:14 +0200

    feat: added pages control by decorators

---
+++ main/decorators.py
@@ -0,0 +1,23 @@
+from django.http import Http404
+from django.shortcuts import redirect
+
+def unauthanticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('main')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+# def allowed_users(allowed_roles=[]):
+#     def decorator(view_func):
+#         def wrapper_func(request, *args, **kwargs):
+#             group = None
+#             if request.user.groups.exists():
+#                 group = request.user.groups.all()[0].name
+#             if group in allowed_roles:
+#                 return view_func(request, *args, **kwargs)
+#             else:
+#                 return Http404()
+#         return wrapper_func
+#     return decorator
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate, login
 from .utils import convert_hex_number_into_cyrilic
+from main.decorators import unauthanticated_user
 
 # Create your views here.
 def main_page(request):
@@ -11,26 +13,24 @@ def main_page(request):
     }) 
 
 def login_page(request):
-    if not request.user.is_authenticated:
-        return redirect(request, 'register')
-    else:
-        if request.method == 'POST':
-            password = request.POST['password']
-            username = request.POST['username']
-            user = authenticate(request, password=password, username=username)
-            if user is not None:
-                login(request, user)
-                return redirect('calendar')
-            else:
-                return redirect('login')
+    if request.method == 'POST':
+        password = request.POST['password']
+        username = request.POST['username']
+        user = authenticate(request, password=password, username=username)
+        if user is not None:
+            login(request, user)
+            return redirect('calendar')
         else:
-            return render(request, 'login_page.html')
+            return redirect('login')
+    else:
+        return render(request, 'login_page.html')
 
+@unauthanticated_user
 def register_page(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user_form = form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
@@ -38,7 +38,6 @@ def register_page(request):
             return redirect('main')
     else:
         form = UserCreationForm()
-
     return render(request, 'register_page.html', {
         'form':form,
         })
